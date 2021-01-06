fe4e9d14079cef01dd5c81ea88cf00da55f91eb7
busra06 <busra.polat.tr@gmail.com>
2021-01-06 15:38:09 +0300

    MVC_viewBag_viewData

---
+++ MVC_Ornek5/Controllers/HomeController.cs
@@ -0,0 +1,36 @@
+﻿using MVC_Ornek5.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC_Ornek5.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            List<Kategori> kategoriler = new List<Kategori>
+            {
+                new Kategori { Id = 1, KategoriAdi = "Beverages", Aciklama = "Soft drinks, coffees, teas, beers, and ales"},
+                new Kategori { Id = 2, KategoriAdi = "Condiments", Aciklama = "Sweet and savory sauces, relishes, spreads, and seasonings"},
+                new Kategori { Id = 3, KategoriAdi = "Confections", Aciklama = "Desserts, candies, and sweet breads"},
+                new Kategori { Id = 4, KategoriAdi = "Dairy Products", Aciklama = "Cheeses"},
+                new Kategori { Id = 5, KategoriAdi = "Grains/Cereals", Aciklama = "Breads, crackers, pasta, and cereal"}
+            };
+
+            ViewData["KategoriListesi"] = kategoriler;
+            ViewBag.KategoriListesi = kategoriler;
+            TempData["KategoriListesi"] = kategoriler;
+
+            return View();
+        }
+
+        public ActionResult Category()
+        {
+            return View();
+        }
+    }
+}
---
+++ MVC_Ornek5/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MVC_Ornek5.MvcApplication" Language="C#" %>
